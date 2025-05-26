1ff48b91d4df8697581e41e1f0c01ca897a7e930
MiguelDeroma <errolawitan@gmail.com>
2025-05-26 08:31:26 +0800

    Some Changes

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 
 // CORS configuration
 app.use(cors({
-    origin: '*', // Allow all origins during development
+    origin: ['https://capstone-server-go3v.onrender.com', 'http://localhost:5000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
     credentials: true
