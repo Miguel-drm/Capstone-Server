15ef3e0d39fbe7490e18b72a19b6cb818c89e3c4
Miguel-drm <mglderoma@gmail.com>
2025-06-03 18:11:56 +0800

    initial commit

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -135,7 +135,7 @@ export default function LoginScreen() {
         disabled={isLoading}
       >
         <Text style={styles.buttonText}>
-          {isLoading ? 'Logging in...' : 'Log in'}
+          {isLoading ? 'Logging in...' : 'Log on'}
         </Text>
       </TouchableOpacity>
       <View style={{ flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center', width: '100%', gap: 10 }}>
