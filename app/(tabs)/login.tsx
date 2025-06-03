a9fa08a926066d4bc2ae78fa9e7c30c359197021
Miguel-drm <mglderoma@gmail.com>
2025-06-03 18:43:57 +0800

    trying merge 2

--- app/(tabs)/login.tsx
+++ app/(tabs)/login.tsx
@@ -145,7 +145,7 @@ export default function LoginScreen() {
         <View style={{ flexDirection: 'row', justifyContent: 'center', alignItems: 'center', gap: 5 }}>
           <Text style={styles.link}>Don't have an account?</Text>
           <TouchableOpacity onPress={() => router.push('/signup')}>
-            <Text style={styles.link}>Sign appppppp</Text>
+            <Text style={styles.link}>Sign up</Text>
           </TouchableOpacity>
         </View>
       </View>
